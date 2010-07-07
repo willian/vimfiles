3d6a9a01a896ac807d7c5cafd393ecc2da0002f5
Willian Fernandes <wfernandes@ikwa.com.br>
2010-07-07 15:51:00 -0300

    added some plugins as git submodules

---
+++ install.sh
@@ -0,0 +1,21 @@
+VIMFILES_PATH=`pwd`
+# Generating the symlinks
+if [ ! -d ~/.vim ]; then
+  ln -sf $VIMFILES_PATH ~/.vim
+fi
+ln -sf "${VIMFILES_PATH}/vimrc" ~/.vimrc
+
+# Updating submodules
+git submodule init
+git submodule update
+
+# Installing pathogen.vim
+if [ ! -d ~/.vim/autoload ]; then
+  mkdir ~/.vim/autoload
+fi
+ln -sf "${VIMFILES_PATH}/bundles/pathogen/autoload/pathogen.vim" ~/.vim/autoload/pathogen.vim 
+
+# Installing Command-T plugin
+cd ~/.vim/bundles/Command-T/ruby/command-t && ruby extconf.rb && make
+
+echo "Done!"
