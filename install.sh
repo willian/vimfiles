25db19b07db31b2a8d8fcc3ecef597584ea65877
Willian Fernandes <willian@willianfernandes.com.br>
2013-05-16 07:25:36 -0300

    Automatically create backup, swap and undo directories.

--- install.sh
+++ install.sh
@@ -9,5 +9,12 @@ ln -sf $CURRENT_PATH $HOME/.vim
 git clone https://github.com/gmarik/vundle.git $CURRENT_PATH/bundle/vundle
 vim +BundleInstall! +qall
 
+unamestr=`uname`
+if [[ "$unamestr" == "Darwin" ]]; then
+  mkdir -p ~/Library/Vim/{swap,backup,undo}
+else
+  mkdir -p ~/.local/share/vim/{swap,backup,undo}
+fi
+
 echo "Vim           [OK]"
 
